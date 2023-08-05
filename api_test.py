bbca9f6746fb8ace72314ef9290c082bfe072211
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2023-08-06 01:50:52 +0900

    Add files via upload

---
+++ api_test.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+#ユーザ登録
+def userregist(addres,passowrd):
+    url = "http://192.168.3.37/api/makeconnection.php"
+
+    postitems = {
+        'address':addres,
+        'pass':passowrd
+    }
+    ps_result = requests.post(url,data=postitems)
+    return ps_result.text
+
+#動画ファイルの送信(20秒,mp4 MPEG-4フォーマットのみ対応)
+def sendMovieData(filepath,uuid):
+    url = "http://procon.schnetworks.net/api/movieobs.php"
+    file = {
+        'file1':open(filepath,"rb")
+    }
+
+    postitems = {
+        'uuid':uuid
+    }
+    ps_result = requests.post(url,data=postitems,files=file)
+    return ps_result.text
+
+sendMovieData("録画した20秒のファイル","makeconnectionで帰ってきたuuid")
